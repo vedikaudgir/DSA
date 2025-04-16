5bde50c7703e00f8a620e9057d91c50818b3af46
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-17 01:49:24 +0530

    Add files via upload

---
+++ trees/preorder/preorderTraversal.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    char data;
+    struct Node* leftChild;
+    struct Node* rightChild;
+}node;
+
+node* createNode(char d)
+{
+    node* newNode = (node*)malloc(sizeof(node));
+
+    newNode->data = d;
+    newNode->leftChild = NULL;
+    newNode->rightChild = NULL;
+
+    return newNode;
+}
+
+void printInorder(node* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    printf("%4c", root->data);
+    printInorder(root->leftChild);
+    printInorder(root->rightChild);
+}
+
+void main()
+{
+    node* head = createNode('A');
+    head->leftChild = createNode('B');
+    head->rightChild = createNode('C');
+    head->leftChild->leftChild = createNode('D');
+    head->leftChild->rightChild = createNode('E');
+    head->rightChild->leftChild = createNode('F');
+    head->rightChild->leftChild->rightChild = createNode('G');
+    printInorder(head);
+}
